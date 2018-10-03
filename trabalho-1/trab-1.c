24a802cbd989b14ee379f73937122bc16b0c6f37
otherpirate <mauro_murari@hotmail.com>
2018-10-03 19:09:08 -0300

    Improve to work with gcc

--- trabalho-1/trab-1.c
+++ trabalho-1/trab-1.c
@@ -11,7 +11,7 @@ a mensagem. Cada consumidor deve receber as mensagens na
 ordem em que foram depositadas.
 */
 
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -23,11 +23,15 @@ int MESSAGES = 3;
 int FALSE = 0;
 int TRUE = 1;
 
-sem_t semaphores[];
-pthread_t threads[];
+sem_t *semaphores[999];
+pthread_t threads[999];
 int values[999];
 int reads[999][999];
 
+
+struct Me {
+    int id;
+};
 
 int is_all_read(int line) {
     for (int i = 0; i < CONSUMERS; i++) {
@@ -56,53 +60,64 @@ void set_not_reads(int line) {
     }
 }
 
-void *write(int me) {
+void *write(void *data) {
+    struct Me *struct_me = data;
+    int me = struct_me->id;
     for (int i = 1; i <= MESSAGES; i++) {
         while (is_all_read(me) == FALSE) {}
-        sem_wait(&semaphores[me]);
+        if (i > 1) {
+            sem_wait(semaphores[me]);
+        }
         printf("P%d - Produzindo...\n", me);
         set_not_reads(me);
         values[me] = i;
         printf("P%d - Produzido: %d\n", me, values[me]);
-        sem_post(&semaphores[me]);
+        sem_post(semaphores[me]);
     }
+    free(struct_me);
+    return NULL;
  }
 
-void *read(int me) {
+void *read(void *data) {
+    struct Me *struct_me = data;
+    int me = struct_me->id;
     while (is_done() == FALSE) {
         for (int producer = 0; producer < PRODUCERS; producer++) {
-            if (values[producer] > 0 && reads[producer][me] == FALSE){
-                sem_wait(&semaphores[me]);
+            if (values[producer] > 0 && reads[producer][me] == FALSE) {
+                sem_wait(semaphores[producer]);
                 printf("C%d - Lendo P%d: %d\n", me, producer, values[producer]);
                 reads[producer][me] = TRUE;
                 printf("C%d - Liberando P%d\n", me, producer);
-                sem_post(&semaphores[producer]);
+                sem_post(semaphores[producer]);
             }
         }
     }
+    free(struct_me);
+    return NULL;
 }
 
 
-void define_values() {
-    printf("Produtores: ");
-    scanf("%d", &PRODUCERS);
-    printf("Consumidores: ");
-    scanf("%d", &CONSUMERS);
-    printf("Mensagens: ");
-    scanf("%d", &MESSAGES);
+void define_values(char *argv[]) {
+    PRODUCERS = atoi(argv[1]);
+    CONSUMERS = atoi(argv[2]);
+    MESSAGES = atoi(argv[3]);
 }
 
-int main(void) {
-    define_values();
+int main(int argc, char *argv[]) {
+    define_values(argv);
     printf("\nP: %d, C: %d, M: %d\n", PRODUCERS, CONSUMERS, MESSAGES);
     int producer, consumer;
+    struct Me *struct_me;
+    sem_t *semaphore;
     for (producer = 0; producer < PRODUCERS; producer++) {
-        sem_t semaphore;
+        semaphore = malloc(sizeof(sem_t));;
         semaphores[producer] = semaphore;
-        sem_init(&semaphore, 0, 0);
+        sem_init(semaphore, 0, 0);
 
         pthread_t thread;
-        pthread_create(&thread, NULL, write, producer);
+        struct_me = malloc(sizeof(struct Me));
+        struct_me->id = producer;
+        pthread_create(&thread, NULL, write, struct_me);
         threads[producer] = thread;
 
         for (int consumer = 0; consumer < CONSUMERS; consumer++) {
@@ -112,7 +127,9 @@ int main(void) {
 
     for (consumer = 0; consumer < CONSUMERS; consumer++) {
         pthread_t thread;
-        pthread_create(&thread, NULL, read, consumer);
+        struct_me = malloc(sizeof(struct Me));
+        struct_me->id = consumer;
+        pthread_create(&thread, NULL, read, struct_me);
         threads[producer + consumer] = thread;
     }
 
@@ -122,7 +139,7 @@ int main(void) {
 
     values[0] = 0;
     for (int producer = 0; producer < PRODUCERS + CONSUMERS; producer++) {
-        sem_destroy(&semaphores[producer]);
+        sem_destroy(semaphores[producer]);
     }
     printf("Done\n");
     return 0;
